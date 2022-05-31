2a130b5cf5de9dfd449cb688da6e35aa924948f8
Kyeongtae Nam <vituscokr@gmail.com>
2022-05-31 16:15:38 +0900

    Binding

--- Sources/YDNDUI/ZoomPhotoView.swift
+++ Sources/YDNDUI/ZoomPhotoView.swift
@@ -14,52 +14,19 @@ public struct ZoomPhotoView : View {
     
     @Binding var index : Int
     @Binding var urls : [String]
-    
-    var scale : CGFloat
+    @Binding var scale : CGFloat
     
     public init(
         index: Binding<Int>,
         urls : Binding<[String]>,
-        scale : CGFloat = 1.0
+        scale : Binding<CGFloat>
     ) {
         self._index = index
         self._urls = urls
-        self.scale = scale 
+        self._scale = scale
     }
     
     public var body : some View {
-        Text("hi")
-    }
-}
-/*
-public struct ZoomPhotoView : View {
-//    @Environment(\.safeAreaInsets) private var safeAreaInsets
-//    @Environment(\.presentationMode) private var presentationMode
-//
-//    @Binding var index : Int
-//    @Binding var urls : [String]
-//    @State var scale : CGFloat = 1.0  //1.0
-    
-    
-//    public init(index: Binding<Int>,
-//         urls : Binding<[String]>,
-//         scale: CGFloat = 1.0
-//    ) {
-//        self._index = index
-//        self._urls = urls
-//        self.scale = scale
-//
-//    }
-    
-    public init() {
-        
-    }
-
-    
-    var body: some View {
-        
-        Text("hi")
-        /*
         ZStack {
             
             Color.black
@@ -105,21 +72,21 @@ public struct ZoomPhotoView : View {
             VStack (spacing:0) {
                 ZStack {
                     HStack(spacing:0) {
-                    Button(action:closeAction) {
-                        EmptyView()
-                    }.buttonStyle(LSSButtonStyle(change: { state in
-                        Image("close_white")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:24, height:24)
-                    }))
-                    .padding(.horizontal, 16)
-                    .frame(height:48)
-                        Spacer()
-                    }
+//                    Button(action:closeAction) {
+//                        EmptyView()
+//                    }.buttonStyle(DDButtonStyle(change: { state in
+//                        Image("close_white")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width:24, height:24)
+//                    }))
+//                    .padding(.horizontal, 16)
+//                    .frame(height:48)
+//                        Spacer()
+//                    }
                     
                     Text("\(index + 1)/\(self.urls.count)")
-                        .apply18Bold()
+//                        .apply18Bold()
                         .foregroundColor(Color.white)
                 }
                 .frame(height:48)
@@ -131,7 +98,6 @@ public struct ZoomPhotoView : View {
         }
         .gesture(MagnificationGesture()
             .onChanged({ scale in
-
                 if scale >= 1 {
                     self.scale = scale
                 }else {
@@ -146,155 +112,23 @@ public struct ZoomPhotoView : View {
                         return
                     }
                     if value.translation.width < 0 {
-                        leftAction()
-                    }
-                    
-                    if value.translation.width > 0 {
-                        rightAction()
-                    }
-//                    if value.translation.height < 0 {
-//                        // up
-//                    }
-//
-//                    if value.translation.height > 0 {
-//                        // down
-//                    }
-                })
-        
-        )
-        .onTapGesture (count:2)  {
-            if self.scale == 1 {
-                self.scale = 2.0
-            }else {
-                self.scale = 1
-            }
-        }
-
-        .edgesIgnoringSafeArea(.all)
-         */
-    }
-    
-    
-}
-
-struct ZoomPhotoView_Previews: PreviewProvider {
-    static var previews: some View {
-        ZoomPhotoView()
-    }
-}
-
-/*
-public struct ZoomPhotoView: View {
-
-    @Environment(\.safeAreaInsets) private var safeAreaInsets
-    @Environment(\.presentationMode) private var presentationMode
-    
-    @Binding var index : Int
-    @Binding var urls : [String]
-    var scale : CGFloat  //1.0
-    
-    
-    public init(index: Binding<Int>,
-         urls : Binding<[String]>,
-         scale: CGFloat = 1.0
-    ) {
-        self._index = index
-        self._urls = urls
-        self.scale = scale
-
-    }
-    
-     public var body: some View {
-        ZStack {
-            
-            Color(hexString: "#000000")
-            GeometryReader { geometry in
-                if self.scale > 1 {
-                    ScrollView ([.vertical, .horizontal], showsIndicators: false){
-                        KFImage(URL(string: urls[index])!)
-                            .resizable()
-                            .scaledToFit()
-                            .scaleEffect(self.scale)
-                            .frame(width: geometry.size.width * self.scale,
-                                   alignment: .center)
-                            .padding(.horizontal, (geometry.size.width * self.scale) - geometry.size.width)
-                            
-                            .padding(.vertical, (geometry.size.width * self.scale) - geometry.size.width)
-
+                        if index >=   self.urls.count - 1 {
+                            index = 0
+                        }else {
+                            index = index  + 1
+                        }
                         
-                    }
-                    .background(Color.clear)
-                }else {
-                    VStack(spacing:0) {
-                        Spacer()
-                        KFImage(URL(string: urls[index])!)
-                            .resizable()
-                            .scaledToFit()
-                            .scaleEffect(self.scale)
-                            .frame(width: geometry.size.width * self.scale,
-                                   alignment: .center)
-                            .padding(.horizontal, (geometry.size.width * self.scale) - geometry.size.width)
-                            
-                            .padding(.vertical, (geometry.size.width * self.scale) - geometry.size.width)
-                        Spacer()
-                    }
-
-
-                }
-//                .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
-                //.scaleEffect(self.scale)
-
-            }
-            
-            VStack (spacing:0) {
-                ZStack {
-                    HStack(spacing:0) {
-                    Button(action:closeAction) {
-                        EmptyView()
-                    }.buttonStyle(LSSButtonStyle(change: { state in
-                        Image("close_white")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:24, height:24)
-                    }))
-                    .padding(.horizontal, 16)
-                    .frame(height:48)
-                        Spacer()
-                    }
-                    
-                    Text("\(index + 1)/\(self.urls.count)")
-                        .apply18Bold()
-                        .foregroundColor(Color.white)
-                }
-                .frame(height:48)
-                .padding(.top, safeAreaInsets.top)
-                Spacer()
-            }
-
-
-        }
-        .gesture(MagnificationGesture()
-            .onChanged({ scale in
-
-                if scale >= 1 {
-                    self.scale = scale
-                }else {
-                    self.scale = 1
-                }
-            }))
-        .gesture(
-            DragGesture(minimumDistance: 10, coordinateSpace: .local)
-                .onEnded({ value in
-                    
-                    guard urls.count > 1 else {
-                        return
-                    }
-                    if value.translation.width < 0 {
-                        leftAction()
+                        self.scale = 1
                     }
                     
                     if value.translation.width > 0 {
-                        rightAction()
+                        if index <=  0 {
+                            index = self.urls.count - 1
+                        }else {
+                            index = index - 1
+                        }
+                        
+                        self.scale = 1
                     }
 //                    if value.translation.height < 0 {
 //                        // up
@@ -316,7 +150,11 @@ public struct ZoomPhotoView: View {
 
         .edgesIgnoringSafeArea(.all)
     }
-}
+    }
+    
 
-*/
- */
+    
+    func closeAction() {
+        self.presentationMode.wrappedValue.dismiss()
+    }
+}
