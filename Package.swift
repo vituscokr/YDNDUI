559059dd772b1ade39041f9dc55241e16fff7560
Kyeongtae Nam <vituscokr@gmail.com>
2022-08-31 12:03:26 +0900

    feature 테스트 중

--- Package.swift
+++ Package.swift
@@ -29,7 +29,8 @@ let package = Package(
                 "Kingfisher"
 //                .product(name: "KingfisherSwiftUI", package: "Kingfisher")
                 //.product(name: "KingfisherSwiftUI", package: "Kingfisher")
-            ]),
+            ],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "YDNDUITests",
             dependencies: ["YDNDUI"]),
